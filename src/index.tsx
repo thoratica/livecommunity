8c2dbdb9a80d5465569917668f6c7048e15a1044
thoratica <me@thoratica.net>
2020-09-04 16:44:50 +0900

    LIVE Community

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import View from './components/View';
 import App from './App';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <>
     <Router>
-      <Route component={App} />
+      <Route exact path="/" component={App} />
+      <Route exact path="/view/:id" component={View} />
     </Router>
   </>,
   document.getElementById('root')
